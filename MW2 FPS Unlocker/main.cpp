9a7e496303e2209aed185af51d2765065bdd7dbf
FLeX <ahjflex@gmail.com>
2016-04-15 16:52:52 +0200

    folder structure

---
+++ MW2 FPS Unlocker/main.cpp
@@ -0,0 +1,30 @@
+//Credits to Fleep for all the help :)
+#include <iostream>
+#include <windows.h>
+#include "functions.h"
+
+char FpsOpCode[] = "\x7D";
+
+void InitiateHooks()
+{
+	DWORD fpsAddy = GetAddy("iw4mp.exe");
+
+	fpsAddy += 0x5F8152C;
+
+	//iw4mp.exe+5F8152C
+	WriteToMemory(fpsAddy, FpsOpCode, 1);
+}
+
+BOOL WINAPI DllMain(
+	HINSTANCE hinstDLL,
+	DWORD fdwReason,
+	LPVOID lpReserved)
+{
+	switch (fdwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		InitiateHooks();
+		break;
+	}
+	return true;
+}
